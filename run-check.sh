2f0d03afa4d3d911510d9c55f4644f3373251a33
Andreas Kloeckner <inform@tiker.net>
2009-06-14 16:31:07 -0400

    Fixes to reg check script.

--- run-check.sh
+++ run-check.sh
@@ -7,9 +7,14 @@ cd /tmp/regression-check
 
 RUN_ID="run-`date +%Y-%m-%d-%H%M`"
 LOGFILE="$RUN_ID.log"
+NOMPI_LOGFILE="$RUN_ID-nompi.log"
+MPI_LOGFILE="$RUN_ID-mpi.log"
 
 if ! test "$1" = "--log-ok"; then
-  if $0 --log-ok $RUN_ID > $LOGFILE 2>&1; then
+  if timelimit $0 --log-ok $RUN_ID > $LOGFILE 2>&1 \
+    && tail -n 1 $NOMPI_LOGFILE | egrep '= [0-9]* passed' \
+    && tail -n 1 $MPI_LOGFILE | egrep '= [0-9]* passed' \
+    ; then
     echo "CHECK OK"
     exit 0
   else
@@ -27,11 +32,19 @@ hostname
 echo "------------------------------------------------------------------"
 
 RUN_ID="$2"
+
+NOMPI_LOGFILE="$RUN_ID-nompi.log"
+MPI_LOGFILE="$RUN_ID-mpi.log"
+
 git clone http://git.tiker.net/trees/repotool.git "$RUN_ID"
 cd "$RUN_ID"
 ./repotool clone http://git.tiker.net/trees/ .git
 ./repotool setup-env
 eval `./repotool env`
 ./repotool install
-python `which py.test` -k -mpi -n 4
-python `which py.test` -k mpi
+
+export LD_LIBRARY_PATH=$HOME/pool/lib
+export PATH=$PATH:$HOME/pool/cuda/bin
+
+python `which py.test` -k -mpi | tee "../$NOMPI_LOGFILE"
+python `which py.test` -k mpi | tee "../$MPI_LOGFILE"
