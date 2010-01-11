fad12665d381e6f97bcf2dcaf47de0ba7e10d949
Andreas Kloeckner <inform@tiker.net>
2010-01-11 17:17:13 +0100

    Make [ev]tags repotool command runnable from subdirectories.

--- list-active-repos.py
+++ list-active-repos.py
@@ -4,11 +4,16 @@ def read_repo_list(name):
     except IOError:
         return ""
 
-all_repos = read_repo_list("subprojects")
-excluded_repos = read_repo_list("excluded-subprojects")
+import sys
+my_dir = sys.argv[1]
+
+from os.path import join
+
+all_repos = read_repo_list(join(my_dir, "subprojects"))
+excluded_repos = read_repo_list(join(my_dir, "excluded-subprojects"))
 
 for x in excluded_repos:
     all_repos.remove(x)
 
 for x in all_repos:
-    print x
+    print join(my_dir, x)
